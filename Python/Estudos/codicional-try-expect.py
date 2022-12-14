916e37a898e5a0e2f7d9a5523f9c54a6254242ba
ikauematos <kaue178hotimail@gmail.com>
2022-12-13 21:32:57 -0300

    atualização

---
+++ Python/Estudos/codicional-try-expect.py
@@ -0,0 +1,21 @@
+def converte_numero(valor):
+    try: 
+        valor = int(valor)
+        return valor
+
+    except ValueError:
+        try:
+            valor = float(valor)
+            return valor
+
+        except ValueError: #Retornando o padrão = NONE
+           pass
+
+while True:
+
+    numero = converte_numero(input("Digite um numero: "))
+
+    if numero is None: 
+        print("Erro; isso não é um numero")
+    else: 
+        print(numero * 5)
--- Python/Estudos/exceçoes-TRY-EXCEPT.py
+++ Python/Estudos/exceçoes-TRY-EXCEPT.py
@@ -3,6 +3,11 @@ def divide (n1,n2):
         raise ValueError("n2 não pode ser 0")
     return n1 /n2
 
-print(divide(5,10))
-    
+
+#Basicamente estamos tratando esta função caso o usuario digite um coisa que não queremos que ele digite
+try:
+    print(divide(n1=2,n2=4))
+except ValueError as error:
+    print("Você esta tentanto dividir um valor zero")
+    #print(error)
 
